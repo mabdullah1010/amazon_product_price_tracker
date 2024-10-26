fb148e09e8be1c2ae3e2b9beff077ca34db5ec5c
Muhammad Abdullah <mabdullah@conncoll.edu>
2024-10-25 23:07:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ MY_EMAIL = os.getenv("MY_EMAIL")
 MY_PASSWORD = os.getenv("MY_PASSWORD")
 RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
 
+
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,"
               "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
